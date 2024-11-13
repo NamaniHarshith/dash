9a704f4b22e53d03f458a056f3cd8f86a273a01a
NamaniHarshith <namaniharshith9@gmail.com>
2024-11-13 22:57:57 +0530

    signup commit

--- dashboad/src/App.js
+++ dashboad/src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SignInForm from './SignInForm';
 import SignupForm from './SignupForm';
 import './App.css';
 
 function App() {
+  const [showSignUp, setShowSignUp] = useState(false);
+
+  const toggleForm = () => {
+    setShowSignUp(!showSignUp);
+  };
+
   return (
-    <div className="signup-container">
+    <div className="auth-container">
       <div className="illustration-section">
         <img src="https://your-illustration-url.com/illustration.png" alt="Sign up illustration" />
-        <h2>Sign Up to</h2>
+        <h2>{showSignUp ? "Sign up to" : "Sign in to"}</h2>
         <p>Lorem Ipsum is simply</p>
-        <p>If you already have an account, <a href="/login">Login here!</a></p>
+        {showSignUp ? (
+          <p>If you already have an account, <span className="toggle-link" onClick={toggleForm}>Login here!</span></p>
+        ) : (
+          <p>If you don’t have an account, <span className="toggle-link" onClick={toggleForm}>Register here!</span></p>
+        )}
       </div>
-      <SignupForm />
+      {showSignUp ? <SignupForm /> : <SignInForm />}
     </div>
   );
 }
